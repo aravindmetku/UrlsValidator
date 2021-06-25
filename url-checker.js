d3b54e78e50fd653518fa5a70375b23bb4bef304
aravindmetku <aravind.metku@leanix.net>
2021-06-25 21:24:32 +0530

    added cache for already checked urls, and retry mechanism for head using get

--- url-checker.js
+++ url-checker.js
@@ -17,11 +17,16 @@ class UrlObj {
 
     async dynamicCheck() {
         try {
-            const request = await axios.head(this.url)
-            return request.status === 200;
+            const headResponse = await axios.head(this.url)
+            return headResponse.status === 200;
         } catch (e) {
-            return false
+            if (e.isAxiosError && e.response.status === 501) {
+                await timeout(10000);
+                let getResponse = await axios.get(this.url);
+                return getResponse.status === 200;
+            }
         }
+        return false
     }
 
     staticCheck() {
@@ -41,23 +46,43 @@ function getFlattenedData(data) {
     return data.data.documents.map(doc => new UrlObj(doc.url, data.id, doc.name));
 }
 
-async function urlChecker(ldifContent) {
-    const flattened = ldifContent.content.flatMap(getFlattenedData)
+async function getAllInvalidUrlObjs(flattened) {
     const final = []
+    const alreadySeen = new Set()
     for (let i = 0; i < flattened.length; i++) {
         const urlObj = flattened[i]
+        if (alreadySeen.has(urlObj.url)) {
+            continue
+        }
+        await timeout(10000)
         console.log('checking: ', urlObj.url)
         const check = await urlObj.check()
         if (!check) {
             final.push(urlObj)
         }
+        alreadySeen.add(urlObj.url)
     }
+    return final;
+}
 
-    console.log('invalid urls', final.length)
-    fs.writeFileSync('output.json', JSON.stringify(final));
-    return final
+async function urlChecker(ldifContent) {
+    const flattened = ldifContent.content.flatMap(getFlattenedData)
+    console.log('Total URLs to check', flattened.length)
+    const checkedInvalidUrls = await getAllInvalidUrlObjs(flattened);
+    console.log('invalid urls ', checkedInvalidUrls.length)
+
+    try{
+        fs.writeFileSync('output.json', JSON.stringify(checkedInvalidUrls));
+    } catch (_) {
+        console.log("failed to save to file")
+    }
+    return checkedInvalidUrls
+}
+
+function timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 console.log(process.argv[2])
-const ldifContent = require(process.argv[2])x
+const ldifContent = require(process.argv[2])
 urlChecker(ldifContent);
